5d8e2660082f6b908f1ce0b41e336f7f15db9826
Your Name <usamax089@gmail.com>
2025-02-27 13:44:46 +0300

    complete

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://donation_user:yourpassword@localhost:5432/donation_platform")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_jwt_secret_key")
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
